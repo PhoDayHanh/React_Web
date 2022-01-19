4a8d77171d8342a72cf20df2609ecd27dc260196
PhoDayHanh <nguyentien05072001@gmail.com>
2022-01-20 00:32:39 +0700

    Functional Component Fix

--- confusion/src/components/DishDetailMini.js
+++ confusion/src/components/DishDetailMini.js
@@ -3,78 +3,79 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "re
 
 
 
-function RenderDish({ dish }) {
 
-    if (dish != null) {
-        return (
-            <div className='col-12 col-md-5 m-1'>
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle> {dish.name}</CardTitle>
-                        <CardText> {dish.description} </CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div></div>
-        );
-    }
-}
+ function RenderDish({dish}) {
 
-function RenderComments({ comments }) {
-    if (comments == null) {
-        return (<div></div>)
+        if (dish != null) {
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}</CardTitle>
+                            <CardText> {dish.description} </CardText>
+                        </CardBody>
+                    </Card>
+                </div>   
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
     }
-    const cmnts = comments.map(comment => {
-        return (
-            <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>-- {comment.author},
+
+
+  function RenderComments({comments}){
+        if (comments == null) {
+            return (<div></div>)
+        }
+        const cmnts = comments.map(comment => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},
                     &nbsp;
                     {new Intl.DateTimeFormat('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: '2-digit'
                     }).format(new Date(comment.date))}
-                </p>
-            </li>
+                    </p>
+                </li>
+            )
+        })
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <h4> Comments </h4>
+                <ul className='list-unstyled'>
+                    {cmnts}
+                </ul>
+
+            </div>
         )
-    })
-
-    return (
-        <div className='col-12 col-md-5 m-1'>
-            <h4> Comments </h4>
-            <ul className='list-unstyled'>
-                {cmnts}
-            </ul>
-
-        </div>
-    );
-}
-
-
-const DishDetailMini = (props) => {
-    console.log("DishDetail Component Render invoked");
-
-    const dish = this.props.dish
-
-    console.log(dish);
-
-    if (dish == null) {
-        return (<div></div>);
     }
 
-    <>
-        <RenderDish dish={props.dish} />
-        <RenderComments comments={props.dish.comments} />
-    </>
+
+    const DishDetailMini = (props) => {
+        const dish = props.dish;
+        const comments = props.comments;
+
+        console.log(dish);
+        
+        if (dish == null) {
+            return (<div></div>);
+        }
 
 
-}
+        return (
+            <div className='row'>
+              <RenderDish dish={props.dish}/>
+              <RenderComments comments={props.dish.comments}/>
+            </div>
+        )
+    }
 
 
 
